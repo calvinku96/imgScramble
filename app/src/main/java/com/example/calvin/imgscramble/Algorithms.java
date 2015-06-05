624510caba5a50b1bacc2e8beef2e9b848cd11c6
Calvin <calvinku96@hotmail.com>
2015-06-05 23:23:31 +0800

    Add the layout for output images, move all algorithm methods to Algorithms.java

---
+++ app/src/main/java/com/example/calvin/imgscramble/Algorithms.java
@@ -0,0 +1,182 @@
+package com.example.calvin.imgscramble;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.net.Uri;
+
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
+/**
+ * Created by calvin on 6/5/15.
+ */
+public class Algorithms {
+    final protected static char[] hexArray = "0123456789abcdef".toCharArray();
+
+    //Methods hashes
+    public static byte[] getHashMultiple(int mul, byte[] pass) {
+        for (int i = 0; i < mul; i++) {
+            pass = getHash(pass);
+        }
+        return pass;
+    }
+
+    public static byte[] getHash(byte[] pass) {
+        //Do as SHA-256 hash once
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.reset();
+            return digest.digest(pass);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        //Convert byte[] array from getHash to hex String
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    // Methods for Lehmer
+    public static int[] encode(int n, BigInteger d) {
+        //Turn the lehmer code d into the falling factorial sequence of number
+        Factorial fact = new Factorial(n);
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            BigInteger fi = fact.factorial(i);
+            BigInteger[] ansarr = d.divideAndRemainder(fi);
+            ans[n - 1 - i] = ansarr[0].intValue();
+            d = ansarr[1];
+        }
+        return ans;
+    }
+
+    public static int[] ffs2seq(int n, int[] input) {
+        //Convert the falling factorial sequence to the combination sequence
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (input[j] >= input[i]) {
+                    input[j] = input[j] + 1;
+                }
+            }
+        }
+        return input;
+    }
+
+
+    // Methods for RearrangeImage AsyncTask
+    public static Bitmap rearrangeImageMethod(ArrayList<Bitmap> imagearray, int[] permarray, boolean scrambleboolean, int row, int col) {
+        Bitmap[] rearrangedimagearray = new Bitmap[imagearray.size()];
+        if (scrambleboolean) {
+            //find inverse permutation
+            int[] invpermarray = new int[permarray.length];
+            for (int i = 0; i < permarray.length; i++) {
+                invpermarray[permarray[i]] = i;
+            }
+            permarray = invpermarray;
+        }
+        //rearrange
+        for (int i = 0; i < imagearray.size(); i++) {
+            rearrangedimagearray[permarray[i]] = imagearray.get(i);
+        }
+        //Combine all into one
+        int indivWidth = rearrangedimagearray[0].getWidth();
+        int indivHeight = rearrangedimagearray[0].getHeight();
+        int totalWidth = col * indivWidth;
+        int totalHeight = row * indivHeight;
+
+        Bitmap temp = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(temp);
+        int yCoord = 0;
+        for (int y = 0; y < row; y++) {
+            int xCoord = 0;
+            for (int x = 0; x < col; x++) {
+                // Add to canvas
+                canvas.drawBitmap(rearrangedimagearray[x + y * col], (float) xCoord, (float) yCoord, null);
+                xCoord += indivWidth;
+            }
+            yCoord += indivHeight;
+        }
+        return temp;
+    }
+
+    //Methods for HashNum AsyncTask
+    public static int[] hashNumMethod(String[] params) {
+        String password = params[0];
+        int row = Integer.parseInt(params[1]);
+        int col = Integer.parseInt(params[2]);
+        int n = row * col;
+        //Generates the lehmer code in base-16 using hashing functions.
+        //Calculate the number of digits we should have
+        //Then calculate n! using the Factorial Class
+        //If the string is greater than n!, then find some way to reproducibly reduce the string
+        //Give out string as a hex representation of BigInteger
+        Factorial fact = new Factorial(0);
+        int numdigits = fact.factorial(n).toString(16).length();
+        //Give initial hash
+        byte[] hash;
+        try {
+            hash = password.getBytes("UTF-8");
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        hash = getHashMultiple(2000, hash);
+        String output = bytesToHex(hash);
+        //Find how long is the lehmer code
+        int multiplier = (numdigits / 64);
+        for (int e = 0; e < multiplier; e++) {
+            int num = Integer.parseInt(output.substring(output.length() - 1), 16);
+            num = num + 1;
+            //Hash it num times
+            hash = getHashMultiple(num, hash);
+            //Add it to the output
+            output = output + bytesToHex(hash);
+        }
+        output = output.substring(0, numdigits - 1);
+
+        //Lehmer
+        BigInteger d = new BigInteger(output, 16);
+        int[] code = encode(n, d);
+        int[] perm = ffs2seq(n, code);
+        return perm;
+    }
+
+    // Methods for SplitImage
+    public static ArrayList<Bitmap> splitImageMethod (String[] params, InputStream imageInputStream){
+        Uri imageuri = Uri.parse(params[0]);
+            int row = Integer.parseInt(params[1]);
+            int col = Integer.parseInt(params[2]);
+            //Store all the image chunks
+            ArrayList<Bitmap> chunkedimages = new ArrayList<Bitmap>(row * col);
+            //Convert Uri to Bitmap
+            try {
+                BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(imageInputStream, false);
+                int chunkHeight = decoder.getHeight() / row;
+                int chunkWidth = decoder.getWidth() / col;
+
+                int yCoord = 0;
+                for (int y = 0; y < row; y++) {
+                    int xCoord = 0;
+                    for (int x = 0; x < col; x++) {
+                        Bitmap tempimage = decoder.decodeRegion(new Rect(xCoord, yCoord, xCoord + chunkWidth, yCoord + chunkHeight), null);
+                        chunkedimages.add(tempimage);
+                        xCoord += chunkWidth;
+                    }
+                    yCoord += chunkHeight;
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            return chunkedimages;
+    }
+}
--- app/src/main/java/com/example/calvin/imgscramble/ScramblingActivity.java
+++ app/src/main/java/com/example/calvin/imgscramble/ScramblingActivity.java
@@ -1,18 +1,23 @@
 package com.example.calvin.imgscramble;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapRegionDecoder;
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.provider.OpenableColumns;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +25,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class ScramblingActivity extends ActionBarActivity {
@@ -37,11 +38,39 @@ public class ScramblingActivity extends ActionBarActivity {
     String rowstring;
     String colstring;
     Bitmap outputimage;
+    boolean scramblingdone = false;
+    Uri imageuri;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrambling);
+        // Init seekBar
+        TextView seekBartext = (TextView) findViewById(R.id.scrambling_quality_text);
+        seekBar = (SeekBar) findViewById(R.id.scrambling_quality_seekBar);
+        seekBar.setProgress(9);
+        seekBartext.setText(getString(R.string.scrambling_quality) + getSeekBarProgress(seekBar));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+                TextView seekBartext = (TextView) findViewById(R.id.scrambling_quality_text);
+                seekBartext.setText(getString(R.string.scrambling_quality) + getSeekBarProgress(seekBar));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // Intent
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String imageuristring = extras.getString("EXTRA_IMAGE");
@@ -52,17 +81,21 @@ public class ScramblingActivity extends ActionBarActivity {
         if (scramblestring.equals("d")) {
             scrambleboolean = true;
         }
-        Uri imageuri = Uri.parse(imageuristring);
-        //ImageView image = (ImageView)findViewById(R.id.imageView);
-        //image.setImageURI(imageuri);
+        imageuri = Uri.parse(imageuristring);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.scrambling_picture_parent_layout);
+        layout.setVisibility(View.GONE);
         new SplitImage().execute(imageuristring, rowstring, colstring);
         new HashNum().execute(scramblepassword, rowstring, colstring);
+    }
+
+    public int getSeekBarProgress(SeekBar seekBar) {
+        return (seekBar.getProgress() + 1) * 5;
     }
 
     public void finishHashSplit() {
         if (imagearraypresent && permarraypresent) {
             RearrangeImage re = new RearrangeImage(imagearray, permarray, scrambleboolean, Integer.parseInt(rowstring), Integer.parseInt(colstring));
-            createToast("Yay");
+            editProgressText(R.string.scrambling_rearrange_image);
             re.execute("");
         }
     }
@@ -84,120 +117,43 @@ public class ScramblingActivity extends ActionBarActivity {
 
         @Override
         protected Bitmap doInBackground(String... params) {
-            Bitmap[] rearrangedimagearray = new Bitmap[imagearray.size()];
-            if (scrambleboolean) {
-                //find inverse permutation
-                int[] invpermarray = new int[permarray.length];
-                for (int i = 0; i < permarray.length; i++) {
-                    invpermarray[permarray[i]] = i;
-                }
-                permarray = invpermarray;
-            }
-            //rearrange
-            for (int i = 0; i < imagearray.size(); i++) {
-                rearrangedimagearray[permarray[i]] = imagearray.get(i);
-            }
-            //Combine all into one
-            int indivWidth = rearrangedimagearray[0].getWidth();
-            int indivHeight = rearrangedimagearray[0].getHeight();
-            int totalWidth = col * indivWidth;
-            int totalHeight = row * indivHeight;
-
-            Bitmap temp = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(temp);
-            int yCoord = 0;
-            for (int y = 0; y < row; y++) {
-                int xCoord = 0;
-                for (int x = 0; x < col; x++) {
-                    // Add to canvas
-                    canvas.drawBitmap(rearrangedimagearray[x + y * col], (float) xCoord, (float) yCoord, null);
-                    xCoord += indivWidth;
-                }
-                yCoord += indivHeight;
-            }
-            return temp;
+            return Algorithms.rearrangeImageMethod(imagearray, permarray, scrambleboolean, row, col);
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmapoutput) {
-            createToast("boo");
             ImageView image = (ImageView) findViewById(R.id.scrambling_output_image);
+            LinearLayout layout = (LinearLayout) findViewById(R.id.scrambling_picture_parent_layout);
+            layout.setVisibility(View.VISIBLE);
             image.setImageBitmap(bitmapoutput);
             outputimage = bitmapoutput;
-
-            //save image
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            bitmapoutput.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
-
-            File f = new File(Environment.getExternalStorageDirectory() + File.separator + "test.jpg");
-            try {
-                f.createNewFile();
-                FileOutputStream fo = new FileOutputStream(f);
-                fo.write(bytes.toByteArray());
-                fo.close();
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-            createToast("Saved");
+            scramblingdone = true;
+            RelativeLayout progresslayout = (RelativeLayout) findViewById(R.id.scrambling_loading_layout);
+            progresslayout.setVisibility(View.GONE);
         }
     }
 
-    public void createToast(String text) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+    public void editProgressText(int text) {
+        TextView loadingtext = (TextView) findViewById(R.id.scrambling_loading);
+        loadingtext.setText(getString(text));
     }
 
     private class HashNum extends AsyncTask<String, Void, int[]> {
         @Override
         protected void onPreExecute() {
-            createToast("Executing Hashum");
+            editProgressText(R.string.scrambling_get_permutation);
         }
 
         @Override
         protected int[] doInBackground(String... params) {
-            String password = params[0];
-            int row = Integer.parseInt(params[1]);
-            int col = Integer.parseInt(params[2]);
-            int n = row * col;
-            //Generates the lehmer code in base-16 using hashing functions.
-            //Calculate the number of digits we should have
-            //Then calculate n! using the Factorial Class
-            //If the string is greater than n!, then find some way to reproducibly reduce the string
-            //Give out string as a hex representation of BigInteger
-            Factorial fact = new Factorial(0);
-            int numdigits = fact.factorial(n).toString(16).length();
-            //Give initial hash
-            byte[] hash;
-            try {
-                hash = password.getBytes("UTF-8");
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-            hash = getHashMultiple(2000, hash);
-            String output = bytesToHex(hash);
-            //Find how long is the lehmer code
-            int multiplier = (numdigits / 64);
-            for (int e = 0; e < multiplier; e++) {
-                int num = Integer.parseInt(output.substring(output.length() - 1), 16);
-                num = num + 1;
-                //Hash it num times
-                hash = getHashMultiple(num, hash);
-                //Add it to the output
-                output = output + bytesToHex(hash);
-            }
-            output = output.substring(0, numdigits - 1);
-
-            //Lehmer
-            BigInteger d = new BigInteger(output, 16);
-            int[] code = encode(n, d);
-            int[] perm = ffs2seq(n, code);
-            return perm;
+            return Algorithms.hashNumMethod(params);
         }
 
         @Override
         protected void onPostExecute(int[] outputs) {
             permarray = outputs;
             permarraypresent = true;
-            createToast("Finished HashNum");
             finishHashSplit();
         }
     }
@@ -205,37 +161,18 @@ public class ScramblingActivity extends ActionBarActivity {
     private class SplitImage extends AsyncTask<String, Void, ArrayList<Bitmap>> {
         @Override
         protected void onPreExecute() {
-            createToast("Execute SplitImage");
+            editProgressText(R.string.scrambling_split_image);
         }
 
         @Override
         protected ArrayList<Bitmap> doInBackground(String... params) {
-            Uri imageuri = Uri.parse(params[0]);
-            int row = Integer.parseInt(params[1]);
-            int col = Integer.parseInt(params[2]);
-            //Store all the image chunks
-            ArrayList<Bitmap> chunkedimages = new ArrayList<Bitmap>(row * col);
-            //Convert Uri to Bitmap
+            imageuri = Uri.parse(params[0]);
             try {
                 InputStream imageInputStream = getContentResolver().openInputStream(imageuri);
-                BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(imageInputStream, false);
-                int chunkHeight = decoder.getHeight() / row;
-                int chunkWidth = decoder.getWidth() / col;
-
-                int yCoord = 0;
-                for (int y = 0; y < row; y++) {
-                    int xCoord = 0;
-                    for (int x = 0; x < col; x++) {
-                        Bitmap tempimage = decoder.decodeRegion(new Rect(xCoord, yCoord, xCoord + chunkWidth, yCoord + chunkHeight), null);
-                        chunkedimages.add(tempimage);
-                        xCoord += chunkWidth;
-                    }
-                    yCoord += chunkHeight;
-                }
+                return Algorithms.splitImageMethod(params, imageInputStream);
             } catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
-            return chunkedimages;
         }
 
         @Override
@@ -243,7 +180,7 @@ public class ScramblingActivity extends ActionBarActivity {
             //Pass the chunkedimages to the scramble image AsyncTask
             imagearray = output;
             imagearraypresent = true;
-            createToast("Split Image Finished");
+            editProgressText(R.string.scrambling_split_image_finish);
             finishHashSplit();
         }
     }
@@ -270,61 +207,97 @@ public class ScramblingActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //Methods for HasHnum
-
-    public static byte[] getHashMultiple(int mul, byte[] pass) {
-        for (int i = 0; i < mul; i++) {
-            pass = getHash(pass);
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
         }
-        return pass;
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 
-    public static byte[] getHash(byte[] pass) {
-        //Do as SHA-256 hash once
+    public byte[] convertImageToJPEG() {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        outputimage.compress(Bitmap.CompressFormat.JPEG, getSeekBarProgress(seekBar), bytes);
+        byte[] imagebytearray = bytes.toByteArray();
+        return imagebytearray;
+    }
+
+    public void scramblingShare(View v) {
+        byte[] imagebytearray = convertImageToJPEG();
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, imagebytearray);
+        shareIntent.setType("image/jpeg");
+        startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.scrambling_send_to)));
+    }
+
+    public void scramblingSaveText(View v) {
+        byte[] imagebytearray = convertImageToJPEG();
+        String base64 = Base64.encodeToString(imagebytearray, Base64.DEFAULT);
+        Time today = new Time(Time.getCurrentTimezone());
+        today.setToNow();
+        String datestring = today.format("%Y-%m%-d-%H:%M:%S");
+        String filename = getFileName(imageuri);
+        filename = "imgScramble_text_" + filename.substring(0, filename.length() - 4) + datestring + "-q" + getSeekBarProgress(seekBar) + ".txt";
+        String sdCard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/imgScramble/";
+        File newdir = new File(sdCard);
+        if (!newdir.exists()) {
+            newdir.mkdirs();
+        }
+        File f = new File(sdCard, filename);
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            digest.reset();
-            return digest.digest(pass);
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(base64.getBytes());
+            fo.close();
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+        Toast.makeText(this, getString(R.string.scrambling_scramblingSaveText_saved), Toast.LENGTH_SHORT).show();
     }
 
-    public static String bytesToHex(byte[] bytes) {
-        //Convert byte[] array from getHash to hex String
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    //Lehmer methods
-    public static int[] encode(int n, BigInteger d) {
-        //Turn the lehmer code d into the falling factorial sequence of number
-        Factorial fact = new Factorial(n);
-        int[] ans = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            BigInteger fi = fact.factorial(i);
-            BigInteger[] ansarr = d.divideAndRemainder(fi);
-            ans[n - 1 - i] = ansarr[0].intValue();
-            d = ansarr[1];
-        }
-        return ans;
-    }
-
-    public static int[] ffs2seq(int n, int[] input) {
-        //Convert the falling factorial sequence to the combination sequence
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-                if (input[j] >= input[i]) {
-                    input[j] = input[j] + 1;
-                }
+    public void scramblingSaveImage(View v) {
+        if (scramblingdone) {
+            //Filename
+            Time today = new Time(Time.getCurrentTimezone());
+            today.setToNow();
+            String datestring = today.format("%Y-%m-%d-%H:%M:%S");
+            String filename = getFileName(imageuri);
+            filename = "imgScramble_" + filename.substring(0, filename.length() - 4) + datestring + "-q" + getSeekBarProgress(seekBar) + ".jpg";
+            byte[] imagebytearray = convertImageToJPEG();
+            //save image
+            String sdCard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/imgScramble/";
+            File newdir = new File(sdCard);
+            if (!newdir.exists()) {
+                newdir.mkdirs();
             }
+            File f = new File(sdCard, filename);
+            try {
+                f.createNewFile();
+                FileOutputStream fo = new FileOutputStream(f);
+                fo.write(imagebytearray);
+                fo.close();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            Toast.makeText(this, getString(R.string.scrambling_scramblingSaveImage_saved), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.scrambling_scramblingSaveImage_not_done), Toast.LENGTH_SHORT).show();
         }
-        return input;
     }
 
 }
